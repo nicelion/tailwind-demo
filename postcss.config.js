a07b8d54b6f51fd9bb709c20b417f807cf371d78
Ian Thompson <ianthompson@nicelion.com>
2024-03-06 18:37:11 -0500

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+/**
+ postcss.config.js
+ tailwind-test
+ 
+ Created by Ian Thompson on February 8th 2024
+ ianthompson@nicelion.com
+ https://www.nicelion.com
+ 
+ Copyright (c) 2024 Nice Lion Technologies LLC. All Rights Reserved.
+ 
+*/
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
